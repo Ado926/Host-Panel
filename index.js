1a603eacea8ff90f4cc0ea54b7f10e9a4ce742a1
SoyMaycol 3 <karatekidamericatv@gmail.com>
2025-06-17 08:38:28 -0500

    Update index.js

--- index.js
+++ index.js
@@ -115,6 +115,10 @@ app.post("/api/register", (req, res) => {
   });
 });
 
+app.get("/", (req, res) => res.sendFile(path.join(__dirname, "public", "index.html")));
+
+app.get("/terminal", (req, res) => res.sendFile(path.join(__dirname, "public", "terminal.html")));
+
 // API de login
 app.post("/api/login", (req, res) => {
   const { username, password } = req.body;
